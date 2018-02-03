6a6bdcb1e980b765d104bdfed1846e15d4fc8a36
gigaparsec <jack.tchimino@gmail.com>
2018-02-03 23:45:11 +0100

    addition of event detection

--- runEverything.m
+++ runEverything.m
@@ -2,24 +2,43 @@
 
 %Run everything
 
-%%load signal
-load('C:\Users\Jack Tchimino\Dropbox\BME\internship\data\signal\14053-05 - 5.mat')
-signalHJ = RAW.ewave;
-fs = RAW.eHz;
+%% load cerebellar signal
+clear all
+clc
+prompt = 'input 1 for cerebellar, 2 for epilepsy: \n';
+selection = input(prompt);
 
+if selection==1
+% load cerebellar signal
+    load('C:\Users\Jack Tchimino\Dropbox\BME\internship\data\signal\14053-05 - 5.mat')
+    signal = RAW.ewave;
+    fs = RAW.eHz;
+    clear RAW S SET TRIG
+elseif selection==2
+% load epilepsy signal
+    load('C:\Users\Jack Tchimino\Dropbox\BME\Thesis\data\seizure data\2011_08_24_TH_THAL_3ch_0008.mat')
+    signal = block.segments{1, 1}.analogsignals{1, 1}.signal;
+    fs = block.segments{1, 1}.analogsignals{1, 1}.sampling_rate;
+    seizureTimes = block.segments{1, 1}.events{1, 1}.times;
+else
+    disp("Error: Try again");
+    return;
+end
+clear prompt selection
+clc
 %% get shorter signal
 
-seconds=10;
+seconds=20;
 % numOfSamples = 300*fs;
 % tenMinSignal = signal(1:numOfSamples);
 % tV = [300/numOfSamples:300/numOfSamples:300];
 % fV = [1/300:1/300:fs];
 numOfSamples = ceil(seconds*fs);
-shortSignal = signalHJ(1:numOfSamples+1);
+shortSignal = signal(1:numOfSamples+1);
 
 prepped = prepareSignal(shortSignal,fs,1);
 
-plot(prepped.TimeVector,prepped.signal)
+%plot(prepped.TimeVector,prepped.signal)
 
 %% Spike Detection
 
@@ -27,14 +46,24 @@ spikes = detectSpikes(prepped.signal,prepped.SamplingFrequency,25,25,0.25,'down'
 
 %% Averaging
 
-averaged = averaging(spikes,'Y');
+averaged = averaging(spikes,'N');
 
 %% Spectrogram/fourier of the whole waveform/of each spike
 % not sure if spectrogram of each spike is useful:
 %   too few samples
 %   can get better results with simple fft
 % spectrogram better at monitoring transient changes in longer signal(?)
-spectrogram(prepped.signal,6000,2000,'power')
-view(-45,65)
+
 % use detrend in spectrogram to remove DC offset and spike at 0Hz
-spectrogram(detrend(prepped.signal),600,400,600,fs,'yaxis')
+spectrogram(detrend(prepped.signal),ceil(0.5*fs),ceil(0.25*fs),ceil(0.5*fs),fs,'yaxis')
+view(-45,65)
+
+%% adaptive segmentation
+% must define: -reference window
+%              -test window
+%              -dissimilarity measure
+
+
+
+
+
